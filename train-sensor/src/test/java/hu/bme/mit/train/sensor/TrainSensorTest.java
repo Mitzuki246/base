13d38def9cf2b8c38dae9e09b65b4dfd930a4f82
Mitzuki246 <fneniko@gmail.com>
2023-04-17 17:54:15 +0200

    Tesztek

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -3,17 +3,53 @@ package hu.bme.mit.train.sensor;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainUser;
+
 import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
 
+    TrainSensorImpl Senzor;
+    TrainUser mocTrainUser;
+    TrainController mocTrainController;
+
     @Before
     public void before() {
-        // TODO Add initializations
+        mocTrainUser = mock(TrainUser.class);
+        mocTrainController = mock(TrainController.class);
+
+        Senzor = new TrainSensorImpl(mocTrainController, mocTrainUser);
+
     }
 
     @Test
-    public void ThisIsAnExampleTestStub() {
-        // TODO Delete this and add test cases based on the issues
+    public void AbsoluteSpeed_Lower() {
+        Senzor.overrideSpeedLimit(-10);
+
+        verify(mocTrainUser, times(1)).setAlarmState(true);
+    }
+
+    @Test
+    public void AbsoluteSpeed_Upper() {
+        Senzor.overrideSpeedLimit(600);
+
+        verify(mocTrainUser, times(1)).setAlarmState(true);;
+    }
+
+    @Test
+    public void RelativeSpeed_Lower() {
+        Senzor.overrideSpeedLimit(1);
+
+        verify(mocTrainUser, times(1)).setAlarmState(true);
+    }
+
+    @Test
+    public void RelativeSpeed_Upper() {
+        Senzor.overrideSpeedLimit(10);
+
+        verify(mocTrainUser, times(1)).setAlarmState(true);
     }
 }
+
